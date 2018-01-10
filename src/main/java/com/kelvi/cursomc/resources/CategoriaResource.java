80832aa74ddff5fc8a991328d26308ae259428d6
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2018-01-10 03:00:44 -0200

    Testando a primeira classe de domínio - Categoria

--- src/main/java/com/kelvi/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kelvi/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.kelvi.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kelvi.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest Está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Jogos");
+		Categoria cat2 = new Categoria(2,"Eletrônicos");
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
+		
 	
 	}
 
