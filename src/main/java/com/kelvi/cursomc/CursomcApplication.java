006f4e280da1d986dafea227f55efa91706cc748
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2018-01-11 09:16:47 -0200

    Criando operacao de instanciacao automatica

--- src/main/java/com/kelvi/cursomc/CursomcApplication.java
+++ src/main/java/com/kelvi/cursomc/CursomcApplication.java
@@ -1,14 +1,34 @@
 package com.kelvi.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
 //Projeto Spring
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.kelvi.cursomc.domain.Categoria;
+import com.kelvi.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
