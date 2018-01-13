17bc0e5d5da0502ee715c8fc968b7b92c38d90d4
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2018-01-13 21:37:17 -0200

    Ajuste no endpoint /categoria/categorias/{id}

--- src/main/java/com/kelvi/cursomc/services/CategoriaService.java
+++ src/main/java/com/kelvi/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.kelvi.cursomc.domain.Categoria;
 import com.kelvi.cursomc.repositories.CategoriaRepository;
+import com.kelvi.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,12 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Categoria obj = repo.findOne(id);
+		if(obj==null) {
+			throw new ObjectNotFoundException("Objeto não encontrado ! Id:"+id
+					+ ", Tipo: "+ Categoria.class.getName());
+			
+		}
+		
 		return obj;
 		
 	}
