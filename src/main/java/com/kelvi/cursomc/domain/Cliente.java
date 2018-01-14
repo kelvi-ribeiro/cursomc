aac2227ad210e6264279b7f1afa33e3e40771774
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2018-01-14 17:54:58 -0200

    Corrigindo os erros nas notacoes JsonBackReference e JsonManagedReference e as trocando pela notacao JsonIgnore pois estava apresentando erros

--- src/main/java/com/kelvi/cursomc/domain/Cliente.java
+++ src/main/java/com/kelvi/cursomc/domain/Cliente.java
@@ -13,10 +13,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.kelvi.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -33,7 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
+	
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -41,7 +39,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
