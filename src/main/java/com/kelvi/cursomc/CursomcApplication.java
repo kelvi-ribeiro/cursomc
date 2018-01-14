c6f9e308bd6e4ee76ed7ab960da9ec9b7fce9333
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2018-01-14 02:53:30 -0200

    Implementando as classes de Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/kelvi/cursomc/CursomcApplication.java
+++ src/main/java/com/kelvi/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.kelvi.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,20 @@ import com.kelvi.cursomc.domain.Cidade;
 import com.kelvi.cursomc.domain.Cliente;
 import com.kelvi.cursomc.domain.Endereco;
 import com.kelvi.cursomc.domain.Estado;
+import com.kelvi.cursomc.domain.Pagamento;
+import com.kelvi.cursomc.domain.PagamentoComBoleto;
+import com.kelvi.cursomc.domain.PagamentoComCartao;
+import com.kelvi.cursomc.domain.Pedido;
 import com.kelvi.cursomc.domain.Produto;
+import com.kelvi.cursomc.domain.enums.EstadoPagamento;
 import com.kelvi.cursomc.domain.enums.TipoCliente;
 import com.kelvi.cursomc.repositories.CategoriaRepository;
 import com.kelvi.cursomc.repositories.CidadeRepository;
 import com.kelvi.cursomc.repositories.ClienteRepository;
 import com.kelvi.cursomc.repositories.EnderecoRepository;
 import com.kelvi.cursomc.repositories.EstadoRepository;
+import com.kelvi.cursomc.repositories.PagamentoRepository;
+import com.kelvi.cursomc.repositories.PedidoRepository;
 import com.kelvi.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -44,6 +52,10 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentorepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -95,6 +107,23 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null,sdf.parse("10/10/17 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO, ped1,6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2,
+												sdf.parse("30/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1,ped2));
+		pagamentorepository.save(Arrays.asList(pagto1,pagto2));
 		
 	}
 }
--- src/main/java/com/kelvi/cursomc/domain/Cliente.java
+++ src/main/java/com/kelvi/cursomc/domain/Cliente.java
@@ -13,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.kelvi.cursomc.domain.enums.TipoCliente;
@@ -38,6 +39,9 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -107,6 +111,14 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -132,6 +144,8 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	
