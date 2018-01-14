59f5a20ca147315445f0c80195015505c9088da4
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2018-01-13 22:46:07 -0200

    Estado e cidade

--- src/main/java/com/kelvi/cursomc/CursomcApplication.java
+++ src/main/java/com/kelvi/cursomc/CursomcApplication.java
@@ -11,8 +11,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kelvi.cursomc.domain.Categoria;
+import com.kelvi.cursomc.domain.Cidade;
+import com.kelvi.cursomc.domain.Estado;
 import com.kelvi.cursomc.domain.Produto;
 import com.kelvi.cursomc.repositories.CategoriaRepository;
+import com.kelvi.cursomc.repositories.CidadeRepository;
+import com.kelvi.cursomc.repositories.EstadoRepository;
 import com.kelvi.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -23,6 +27,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired	
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository  cidadeRepository; 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -43,11 +53,28 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat1));	
 		
 		
 		categoriaRepository.save(Arrays.asList(cat1,cat2));
 		produtoRepository.save(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null,"Ubelândia",est1);		
+		Cidade c2 = new Cidade(null,"São Paulo",est2);
+		Cidade c3 = new Cidade(null,"Campinas",est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.save(Arrays.asList(est1,est2));
+		
+		cidadeRepository.save(Arrays.asList(c1,c2,c3));
+		
+		
+		
 		
 	}
 }
---
+++ src/main/java/com/kelvi/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.kelvi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kelvi.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
